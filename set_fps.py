06a2b800f31710bd1a5e164fce6e5262b4c5e82e
Grant Robert Thompson <58478508+Tower5954@users.noreply.github.com>
2022-11-05 00:40:58 +0000

    Add files via upload

--- set_fps.py
+++ set_fps.py
@@ -1,24 +1,23 @@
-# install opencv-python version: 4.5.5
 import cv2
 import time
 
-print(cv2.__version__)
 
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FPS, 30)
+print("FPS", cap.get(cv2.CAP_PROP_FPS))
 
-# find out how fast the webcam is. 30fps is the goal
 last_time = time.time()
-while True:
-    _, frame = cap.read(0)
 
-    # resize the frame
+while True:
+    _, frame = cap.read()
     frame = cv2.resize(frame, (640, 480))
 
     text = "FPS: " + str(int(1/(time.time() - last_time)))
     last_time = time.time()
+    cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 2)
 
-    cv2.putText(frame, text, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 125, 0), 2)
     cv2.imshow("Webcam", frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
+
